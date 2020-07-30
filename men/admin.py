a46bb64a9549940b0f08f299c2ae90dd4477781f
Muhammad Foda <mohamad.foda@3segypt.com>
2020-07-30 15:37:21 +0200

    add men models

--- men/admin.py
+++ men/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Men
+
+
+
+admin.site.register(Men)
---
+++ men/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+STATUS_TYPE = (
+    ('Available','Available'),
+    ('Not available','Not available'),
+)
+
+
+# Create your models here.
+class Men(models.Model):
+    title = models.CharField(max_length=100)
+    status = models.CharField(max_length=15, choices=STATUS_TYPE)
+    description = models.TextField(max_length=1000)
+    published_at = models.DateTimeField(auto_now=True)
+    quantity = models.IntegerField(default=1)
+    discount = models.CharField(max_length=50)
+    price_now = models.IntegerField(default=0)
+    Price_before = models.IntegerField(default=0)
+    #size = models.ForeignKey('Size', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
